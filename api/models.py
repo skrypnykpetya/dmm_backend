c857a9983a85c404e6ec1fcc50e80c4cc71bd6d4
Petya <skrypnykpetya@gmail.com>
2022-04-23 11:21:25 +0300

    backend

---
+++ api/models.py
@@ -0,0 +1,326 @@
+from django.db import models
+from users.models import Users
+
+
+class Trailers(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    model = models.CharField(max_length=255)
+    trailer_number = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    license_plate = models.CharField(max_length=255)
+    license_plate_expiration = models.DateField()
+    vehicle_id_number = models.CharField(max_length=255)
+    dot_expiration = models.DateField(blank=True, null=True)
+    inspection_expiration = models.DateField(blank=True, null=True)
+    active = models.IntegerField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'trailers'
+
+
+class Drivers(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    address = models.CharField(max_length=255)
+    active = models.IntegerField()
+    status = models.CharField(max_length=7)
+    license_type = models.CharField(max_length=255)
+    license_expires_at = models.DateField()
+    driver_since_year = models.CharField(max_length=255)
+    years_experience = models.CharField(max_length=255)
+    cdl_number = models.CharField(max_length=255)
+    dot_medical_expiration = models.DateField(blank=True, null=True)
+    license_expiration = models.DateField(blank=True, null=True)
+    operator = models.ForeignKey(Users, related_name='operator', on_delete=models.DO_NOTHING, blank=True, null=True)
+    trailer = models.ForeignKey(Trailers, related_name='trailers', on_delete=models.DO_NOTHING, blank=True, null=True)
+    power_unit = models.ForeignKey('PowerUnits', related_name='power_units', on_delete=models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(Users, related_name='user', on_delete=models.DO_NOTHING, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'drivers'
+
+
+class FailedJobs(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    uuid = models.CharField(unique=True, max_length=255)
+    connection = models.TextField()
+    queue = models.TextField()
+    payload = models.TextField()
+    exception = models.TextField()
+    failed_at = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'failed_jobs'
+
+
+
+class Customers(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    line1 = models.CharField(max_length=255)
+    line2 = models.CharField(max_length=255, blank=True, null=True)
+    zip = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    mcnumber = models.CharField(db_column='mcNumber', max_length=255)  # Field name made lowercase.
+    currency = models.CharField(max_length=255)
+    debtorid = models.PositiveBigIntegerField(db_column='debtorId', blank=True, null=True)  # Field name made lowercase.
+    areoriginalsrequired = models.IntegerField(db_column='areOriginalsRequired')  # Field name made lowercase.
+    isduplicatematchdebtortext = models.IntegerField(db_column='isDuplicateMatchDebtorText')  # Field name made lowercase.
+    isfirstuse = models.IntegerField(db_column='isFirstUse')  # Field name made lowercase.
+    lastuseddate = models.DateField(db_column='lastUsedDate', blank=True, null=True)  # Field name made lowercase.
+    weight = models.ForeignKey('DictionaryValues', related_name='weight', on_delete=models.DO_NOTHING, blank=True, null=True)
+    distance = models.ForeignKey('DictionaryValues', related_name='distance', on_delete=models.DO_NOTHING, blank=True, null=True)
+    temperature = models.ForeignKey('DictionaryValues', related_name='temperatur', on_delete=models.DO_NOTHING, blank=True, null=True)
+    private_note = models.TextField(blank=True, null=True)
+    public_note = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'customers'
+
+class CustomerContacts(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    customer = models.ForeignKey('Customers', on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+    fax = models.CharField(max_length=255, blank=True, null=True)
+    primary = models.IntegerField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'customer_contacts'
+
+
+
+
+class Addresses(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    address = models.CharField(max_length=255)
+    formatted_address = models.CharField(unique=True, max_length=255, blank=True, null=True)
+    addressline = models.CharField(db_column='addressLine', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    admindistrict = models.CharField(db_column='adminDistrict', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    admindistrict2 = models.CharField(db_column='adminDistrict2', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    countryregion = models.CharField(db_column='countryRegion', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    city = models.CharField(max_length=255, blank=True, null=True)
+    zip_code = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'addresses'
+
+class AttachmentRelations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    instance_type = models.CharField(max_length=255)
+    instance_id = models.PositiveBigIntegerField()
+    attachment_id = models.PositiveBigIntegerField()
+    type = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'attachment_relations'
+
+
+class Attachments(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user_id = models.PositiveBigIntegerField()
+    original_name = models.CharField(max_length=255)
+    stored_name = models.CharField(max_length=255)
+    size = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'attachments'
+
+
+class Cities(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    state = models.ForeignKey('States', related_name='states',  on_delete=models.DO_NOTHING)
+    county = models.ForeignKey('Counties', related_name='countys', on_delete=models.DO_NOTHING)
+    lat = models.DecimalField(max_digits=7, decimal_places=4)
+    lng = models.DecimalField(max_digits=7, decimal_places=4)
+    aid = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'cities'
+
+
+class Counties(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    state = models.ForeignKey('States', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'counties'
+
+class Dictionaries(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'dictionaries'
+
+
+class DictionaryValues(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    dictionary = models.ForeignKey(Dictionaries, on_delete=models.DO_NOTHING)
+    code = models.CharField(max_length=255, blank=True, null=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'dictionary_values'
+
+
+class Distances(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    distance = models.FloatField()
+    from_field = models.ForeignKey(Addresses, related_name='from_field', on_delete=models.DO_NOTHING, db_column='from_id')  # Field renamed because it was a Python reserved word.
+    to = models.ForeignKey(Addresses, related_name='to', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'distances'
+
+
+class Invoices(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    load_id = models.PositiveBigIntegerField()
+    status = models.CharField(max_length=10)
+    invoice_id = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'invoices'
+
+
+class Loads(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    driver = models.ForeignKey('Drivers', related_name='driver', on_delete=models.DO_NOTHING, blank=True, null=True)
+    power_unit = models.ForeignKey('PowerUnits', related_name='power_unit', on_delete=models.DO_NOTHING, blank=True, null=True)
+    trailer = models.ForeignKey('Trailers', related_name='trailer', on_delete=models.DO_NOTHING, blank=True, null=True)
+    customer = models.ForeignKey(Customers, related_name='customer', on_delete=models.DO_NOTHING, blank=True, null=True)
+    pickup_address = models.ForeignKey(Addresses, related_name='pickup', on_delete=models.DO_NOTHING, db_column='pickUp_address_id')  # Field name made lowercase.
+    delivery_address = models.ForeignKey(Addresses, related_name='delivery', on_delete=models.DO_NOTHING)
+    order_number = models.CharField(max_length=255)
+    distance = models.IntegerField()
+    tonu = models.IntegerField()
+    price = models.CharField(max_length=255)
+    netamount = models.IntegerField(db_column='netAmount')  # Field name made lowercase.
+    advance = models.IntegerField(blank=True, null=True)
+    advance_fee = models.IntegerField(blank=True, null=True)
+    lumper_fee = models.IntegerField(blank=True, null=True)
+    detention_pay = models.IntegerField(blank=True, null=True)
+    deduction = models.IntegerField(blank=True, null=True)
+    price_other = models.IntegerField(blank=True, null=True)
+    pickup_date = models.DateField(db_column='pickUp_date')  # Field name made lowercase.
+    delivery_date = models.DateField()
+    pickup_time = models.CharField(db_column='pickUp_time', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    delivery_time = models.CharField(max_length=255, blank=True, null=True)
+    delivery_status = models.CharField(max_length=10)
+    payment_status = models.CharField(max_length=9)
+    driver_status = models.CharField(max_length=10)
+    reference_number = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'loads'
+
+class PowerUnits(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    model = models.CharField(max_length=255)
+    pu_number = models.CharField(max_length=255)
+    license_plate = models.CharField(max_length=255)
+    license_plate_expiration = models.DateField()
+    vehicle_id_number = models.CharField(max_length=255)
+    dot_expiration = models.DateField(blank=True, null=True)
+    inspection_expiration = models.DateField(blank=True, null=True)
+    trailer = models.ForeignKey(Trailers, on_delete=models.DO_NOTHING, blank=True, null=True)
+    active = models.IntegerField()
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'power_units'
+
+class States(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'states'
+
+class Zips(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    zip = models.TextField()
+    city = models.ForeignKey(Cities, related_name='city', on_delete=models.DO_NOTHING)
+    state = models.ForeignKey(States, related_name='state', on_delete=models.DO_NOTHING)
+    county = models.ForeignKey(Counties, related_name='county', on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'zips'
+
---
+++ users/models.py
@@ -0,0 +1,158 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+
+        extra_fields.setdefault('is_active', True)
+
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(unique=True, max_length=255)
+    email_verified_at = models.DateTimeField(blank=True, null=True)
+    password = models.CharField(max_length=255)
+    status = models.CharField(max_length=10)
+    remember_token = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class UsersData(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(unique=True, max_length=255)
+    email_verified_at = models.DateTimeField(blank=True, null=True)
+    password = models.CharField(max_length=255)
+    status = models.CharField(max_length=10)
+    remember_token = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'users'
+
+class PasswordResets(models.Model):
+    email = models.CharField(max_length=255)
+    token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'password_resets'
+
+
+class Permissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'permissions'
+
+
+class PersonalAccessTokens(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    tokenable_type = models.CharField(max_length=255)
+    tokenable_id = models.PositiveBigIntegerField()
+    name = models.CharField(max_length=255)
+    token = models.CharField(unique=True, max_length=64)
+    abilities = models.TextField(blank=True, null=True)
+    last_used_at = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'personal_access_tokens'
+
+
+class Roles(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    slug = models.CharField(unique=True, max_length=255)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'roles'
+
+
+class RolesPermissions(models.Model):
+    role = models.OneToOneField(Roles, on_delete=models.DO_NOTHING, primary_key=True)
+    permission = models.ForeignKey(Permissions, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'roles_permissions'
+        unique_together = (('role', 'permission'),)
+
+
+
+class UsersPermissions(models.Model):
+    user = models.OneToOneField(Users, models.DO_NOTHING, primary_key=True)
+    permission = models.ForeignKey(Permissions, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'users_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class UsersRoles(models.Model):
+    user = models.OneToOneField(Users, models.DO_NOTHING, primary_key=True)
+    role = models.ForeignKey(Roles, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'users_roles'
+        unique_together = (('user', 'role'),)
